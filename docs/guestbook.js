509b29bed591e66d836f9fa214f6f5dfca2f9f5a
Eslam-Mohamed50 <30962441+Eslam-Mohamed50@users.noreply.github.com>
2019-08-29 20:00:31 +0200

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/CHANGEME/guestbook';
+const apiUrl = 'https://ac7af337.eu-gb.apiconnect.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
