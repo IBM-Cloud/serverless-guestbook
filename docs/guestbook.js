66af5f037d56d1ce44c036d6aa4eadc5e8e225d3
urpi142019 <57529154+urpi142019@users.noreply.github.com>
2019-11-08 12:32:20 -0500

    yes
    :o

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/CHANGEME/guestbook';
+const apiUrl = 'https://guestbook-api.mydomain.com/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
