91ec0f7dbf1b785800bc4fb1f69e26c9a29d0174
Frederic Lavigne <l2fprod@users.noreply.github.com>
2019-12-19 16:42:18 -0600

    Fixes #7

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/CHANGEME/guestbook';
+const apiUrl = 'https://CHANGEME.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
