9549197eaf460a2c12c9eb9385fe5de3b840f2ca
ecarreon77 <109948244+ecarreon77@users.noreply.github.com>
2022-08-04 13:19:22 +0800

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -7,7 +7,7 @@ const guestbook = {
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}/entries`,
+      url: `$https://c1d69133.us-south.apigw.appdomain.cloud/guestbook/entries`,
       dataType: 'json'
     });
   },
@@ -16,7 +16,7 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: `${apiUrl}/entries`,
+      url: `$https://c1d69133.us-south.apigw.appdomain.cloud/guestbook/entries`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
