9af5e7d1f54f75975c7e49eebef741dca09b5092
Henrik Loeser <data-henrik@users.noreply.github.com>
2022-12-20 17:02:43 +0100

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,6 +1,7 @@
 /**
  * Web application
  */
+// if copying over a full URL, make sure it does NOT end on a /
 const apiUrl = 'https://CHANGEME.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
